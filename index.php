71d0580cc91919ff78e19cd2a801e9f401849df1
Dekna <kdkrisnaaa@gmail.com>
2024-03-24 20:32:42 +0800

    feat(navbar): styling navbar

--- index.php
+++ index.php
@@ -6,8 +6,20 @@
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-  <h1 class="text-3xl font-bold underline text-blue-500">
-    Hello world!
-  </h1>
+  <nav class="bg-blue-500 py-4 flex items-center justify-between px-[15%]">
+    <div class="w-[20%]">
+        <h1 class="font-bold text-xl text-white">E-Laundry</h1>
+    </div>
+    <div class="flex gap-6 items-center justify-center text-white w-[60%]">
+        <a href="#" class="hover:text-black hover:scale-105">Home</a>
+        <a href="#" class="hover:text-black hover:scale-105">Services</a>
+        <a href="#" class="hover:text-black hover:scale-105">Testimoni</a>
+        <a href="#" class="hover:text-black hover:scale-105">About Us</a>
+        <a href="#" class="hover:text-black hover:scale-105">Contact</a>
+    </div>
+    <div class="w-[20%] flex justify-center items-center">
+        <a href="#" class="bg-blue-700 text-white pl-5 pr-5 pt-2 pb-2 rounded hover:bg-blue-600 hover:shadow-md">Login</a>
+    </div>
+  </nav>
 </body>
 </html>
