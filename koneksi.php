75bc148ed39c91690fb7ab26afec335a3851b69d
deknaa <kadekna20@gmail.com>
2024-03-26 19:58:34 +0800

    feat(koneksi): add polymorphism, inheritance, interface

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,17 @@
 <?php
 
+// penerapan namespace
 namespace Connection;
 
-class Database {
+// Interface untuk database (penerapan interface)
+interface DatabaseInterface {
+    public function getConnection();
+    public function closeConnection();
+}
+
+// penerapan class
+class Database implements DatabaseInterface {
+    // menerapkan properties
     private $host = "localhost";
     private $username = "root";
     private $password = "";
@@ -10,7 +19,7 @@ class Database {
 
     protected $koneksi;
 
-    // konstruktor untuk class dari database
+    // konstruktor untuk class dari database (menerapkan prosedur)
     public function __construct() {
         $this->koneksi = new \mysqli($this->host, $this->username, $this->password, $this->database);
 
@@ -19,14 +28,32 @@ class Database {
         }
     }
 
-    // method untuk mendapatkan objek koneksi database
+    // fungsi untuk mendapatkan objek koneksi database
     public function getConnection() {
         return $this->koneksi;
     }
 
-    // method untuk tutup database
-    public function closeDatabase(){
+    // method untuk tutup database (menerapkan method sekaligus prosedur)
+    public function closeConnection(){
         $this->koneksi->close();
     }
 }
+
+//menerapkan inheritance
+class ExtendedDatabase extends Database {
+    // Method tambahan untuk class ExtendedDatabase
+    public function backupDatabase() {
+        // Mendapatkan koneksi database
+        $koneksi = $this->getConnection();
+
+        echo "Backup database berhasil!";
+    }
+
+    // Overriding method closeConnection dari Database (menerapkan konsep polymorphism)
+    public function closeConnection() {
+        parent::closeConnection(); // Memanggil method closeConnection dari class induk
+        echo "Koneksi ditutup secara manual."; // Menambahkan pesan tambahan
+    }
+}
+
 ?>
